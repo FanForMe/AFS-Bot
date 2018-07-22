3e46635d69d89b1022c86d5bbe6a89115b85c384
iTzFanForMe <41344206+iTzFanForMe@users.noreply.github.com>
2018-07-22 16:31:14 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -369,6 +369,18 @@ client.on('message', message => {
    });
 
 
-
+client.on('message', message => {
+            if (message.content.startsWith(prefix + "=bot")) {
+     let embed = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)
+.addField(' السيرفرات🌐',`[${client.guilds.size}]  `)
+.addField(' الاعضاء👥 ',` [${client.users.size}] `)
+.addField('الرومات📚 ',`[${client.channels.size}]`) 
+.addField(' البنق🚀 ',`[${Date.now() - message.createdTimestamp}]`) 
+.addField('مصمم  + صاحب البوت ',`iTzFanForMe`)
+.setColor('#7d2dbe')
+  message.channel.sendEmbed(embed);
+    }
+});
 
 client.login(process.env.BOT_TOKEN);
