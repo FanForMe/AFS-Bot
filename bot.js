32a7ad8868f23cddf3cb2fc284058a9f461140c6
iTzFanForMe <41344206+iTzFanForMe@users.noreply.github.com>
2018-07-22 12:37:49 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,7 +58,7 @@ const N9e7h = [
 'كل شيء يبدأ صغيراً ثم يكبر إلا المصيبة فإنها تبدأ كبيرة ثم تصغر .',
 ]
 client.on('message', message => {
-    if (message.content.startsWith("=نصيحة")) {
+    if (message.content.startsWith('=نصيحة')) {
         if (!message.channel.guild) return message.reply('** هاذا الأمر فقط للسيرفرات **');
         var client = new Discord.RichEmbed()
             .setTitle("نصائح ..")
