eba5182d1debe084332a83400ea8d5f62bd4001a
iTzFanForMe <41344206+iTzFanForMe@users.noreply.github.com>
2018-07-26 10:00:05 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -359,59 +359,6 @@ client.on('message', message => {
 
 
 
-let points = JSON.parse(fs.readFileSync('./3lm/3lmPTS.json', 'utf8'));
-var prefix = "=";
-client.on('message', message => {
-if (!points[message.author.id]) points[message.author.id] = {
-    points: 0,
-  };
-if (message.content.startsWith(prefix + '=علم')) { 
-    if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
-
-const type = require('./3lm/3lm.json');
-const item = type[Math.floor(Math.random() * type.length)];
-const filter = response => {
-    return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
-};
-message.channel.send('**لديك 15 ثانيه لتعرف علم اي دولة**').then(msg => { 
-
-msg.channel.sendFile(`${item.image}`).then(() => {
-        message.channel.awaitMessages(filter, { maxMatches: 1, time: 15000, errors: ['time'] })
-        .then((collected) => {
-        message.channel.send(`${collected.first().author} ✅ **كفوو والله يا شنب اجابتك صحيحه**`);
-        console.log(`[Typing] ${collected.first().author} typed the word.`);
-            let userData = points[message.author.id];
-            userData.points++;
-          })
-          .catch(collected => {
-            message.channel.send(`**تم الانتهاء من الوقت  حظ اوفر المره القادمه :stopwatch: الاجابه هي : __${item.answers}__ **`);
-            console.log('[Typing] Error: No one type the word.');
-          })
-        })
-    })
-}
-});
-
-client.on('message', message => {
-if (message.content.startsWith(prefix + '=نقاطي')) {
-    if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
-    let userData = points[message.author.id];
-    let embed = new Discord.RichEmbed()
-    .setAuthor(`${message.author.tag}`, message.author.avatarURL)
-    .setColor('#000000')
-    .setDescription(`نقاطك: \`${userData.points}\``)
-    message.channel.sendEmbed(embed)
-  }
-  fs.writeFile("./3lm/3lmPTS.json", JSON.stringify(points), (err) => {
-    if (err) console.error(err)
-  })
-});
-
-
-
-
-
-
 client.on('message', message => {
             if (message.content.startsWith(prefix + "=help")) {
      let embed = new Discord.RichEmbed()
