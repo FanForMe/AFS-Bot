b2a8d59eea6f648c0bf1e68b7104ba1cf1003241
iTzFanForMe <41344206+iTzFanForMe@users.noreply.github.com>
2018-09-05 20:03:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,131 +7,16 @@ client.on('ready', () => {
 
 
 
- client.on('guildMemberAdd', member => {
-    var embed = new Discord.RichEmbed()
-    .setAuthor(member.user.username, member.user.avatarURL)
-    .setThumbnail(member.user.avatarURL)
-    .setTitle(`عضو جديد`)
-    .setDescription(`اهلا بك في السيرفر`)
-    .addField(' :bust_in_silhouette:  انت رقم',`**[ ${member.guild.memberCount} ]**`,true)
-    .setColor('GREEN')
-    .setFooter('The King Bot', 'https://cdn.discordapp.com/icons/390551815072251904/418fa2788d8115808951c9881ba8f190.jpg')
-
-var channel =member.guild.channels.find('name', 'wlc')
-if (!channel) return;
-channel.send({embed : embed});
-});
-
-  client.on('typingStart', (ch, user) => {
-    if(user.presence.status === 'offline') {
-        
-        ch.send(`${user} هاهاهاا , كشفتك وانت تكتب ي اوف لاين`)
-        .then(msg => {
-            msg.delete(10000)
-        })
-    }
-})
-  
-  
-  
-  client.on('message', message => {
-    if(message.content.startsWith(prefix + '!#move all')) {
-     if (!message.member.hasPermission("MOVE_MEMBERS")) return message.channel.send('**لايوجد لديك صلاحية سحب الأعضاء**');
-       if(!message.guild.member(client.user).hasPermission("MOVE_MEMBERS")) return message.reply("**لايوجد لدي صلاحية السحب**");
-    if (message.member.voiceChannel == null) return message.channel.send(`**الرجاء الدخول لروم صوتي**`)
-     var author = message.member.voiceChannelID;
-     var m = message.guild.members.filter(m=>m.voiceChannel)
-     message.guild.members.filter(m=>m.voiceChannel).forEach(m => {
-     m.setVoiceChannel(author)
-     })
-     message.channel.send(`**تم سحب جميع الأعضاء الي الروم الصوتي حقك.**`)
-
-
-     }
-       });
-	   
-	    KiNg66S.on('message', function(KiNg66S) {
-if (KiNg66S.author.bot) return;
-if (KiNg66S.author.id === KiNg66S.user.id) return;
-if (KiNg66S.author.equals(KiNg66S.user)) return;
-if (!KiNg66S.content.startsWith(prefix)) return;
-
-var args = KiNg66S.content.substring(prefix.length).split(' ');
-switch (args[0].toLocaleLowerCase()) {
-case ":مسح" :
-KiNg66S.delete()
-if(!KiNg66S.channel.guild) return
-if(KiNg66S.member.hasPermissions(0x2000)){ if (!args[1]) {
-KiNg66S.channel.fetchMessages()
-.then(messages => {
-KiNg66S.channel.bulkDelete(messages);
-var messagesDeleted = messages.array().length;
-KiNg66S.channel.sendMessage(' '+ " " + messagesDeleted + " " +  '**: عدد الرسائل التي تم مسحه**').then(m => m.delete(2500));
-})
-} else {
-let messagecount = parseInt(args[1]);
-KiNg66S.channel.fetchMessages({limit: messagecount}).then(messages => KiNg66S.channel.bulkDelete(messages));
-KiNg66S.channel.sendMessage(' '+ " " + args[1] + " " +  '**: عدد الرسائل التي تم مسحه**').then(m => m.delete(2500));
-KiNg66S.delete(60000);
-}
-} else {
-var manage = new Discord.RichEmbed()
-.setDescription('You Do Not Have Permission MANAGE_MESSAGES :(')
-.setColor("RANDOM")
-KiNg66S.channel.sendEmbed(manage)
-return;
-}
-}
-});
-  
-  
-    client.on('guildCreate', rebel => {
-let Rebel = rebel.guild.owner;
-let codes = rebel.roles.find('name', 'member');
-let join = [`شرفتنآآ بدخول بوت ${client.tag}
-إلى سيرفرك المحترم ${rebel.guild.name}
-سبورت البوت <الرابط هنا>ءء`]
-rebel.guilds.get("أيدي سيرفر السبورت").guild.member(Rebel).addRole(codes)
-rebel.guild.owner.send(join)
-});
-
-
-  client.on('message', message => {
-   if(message.content.startsWith(prefix + "!#invites")) {
-    message.guild.fetchInvites().then(invs => {
-      let user = message.mentions.users.first() || message.author
-      let personalInvites = invs.filter(i => i.inviter.id === user.id);
-      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
-               let mmmmEmbed = new Discord.RichEmbed()
-                         .setAuthor(client.user.username)
-                         .setThumbnail(message.author.avatarURL)
- .addField(` لقد قمت بدعوة :`, ` ${inviteCount} `)
-           .setFooter(`- Requested By: ${message.author.tag}`);
-           message.channel.send(mmmmEmbed)
-});
-  }
-});
-
-
-
-  client.on('message', message => {
-    if (message.author.bot) return;
-     if (message.content === prefix + "!#bot") {
-
-
- message.author.sendMessage(`
- 
- __~~Server Bot~~__
- ???????????????????????????
-? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? 
- __created By__: iTzFanForMe
-
-`);
-
-message.channel.send('**تم الارسال في الخاص**');
-
-    }
-});
-  
+  const devs = ["436535469392527380"];
+const adminprefix = [" =play "];
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developer.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+	  
+	  
 
 client.login(process.env.BOT_TOKEN);
