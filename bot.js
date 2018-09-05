fc6da13ea6c909b74c354ac63ae6dc15da6b240c
iTzFanForMe <41344206+iTzFanForMe@users.noreply.github.com>
2018-09-05 21:41:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,16 +7,29 @@ client.on('ready', () => {
 
 
 
-  const devs = ["436535469392527380"];
-const adminprefix = [" =play "];
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developer.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**✅   ${argresult}**`)
-	  
+client.on('message',async msg => {
+  var p = "!";
+  if(msg.content.startsWith(p + "!#user")) {
+  if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **go play minecraft**');
+  if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');
+  msg.guild.createChannel(`يتم تحضير الروم :[]` , 'voice').then(time => {
+    time.overwritePermissions(msg.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+  setInterval(() => {
+      var currentTime = new Date(),
+Year = currentTime.getFullYear(),
+Month = currentTime.getMonth() + 1,
+Dat = currentTime.getDate()
+      time.setName(`Members : ◤ → ${client.users.size} ← ◢`);
+ },1000);
+  });
+  }
+ 
+});
+
+	 
 	  
 
 client.login(process.env.BOT_TOKEN);
